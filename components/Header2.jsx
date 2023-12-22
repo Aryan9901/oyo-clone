bfebf095a56a527712135fe34afeac6816aa8736
Aryan9901 <911aaryan@gmail.com>
2023-12-22 16:35:09 +0530

    added arrow symbol in Header2 component

--- components/Header2.jsx
+++ components/Header2.jsx
@@ -14,7 +14,11 @@ const Header2 = () => {
 		<div>
 			<div className="flex px-10 py-2 bg-gray-200 justify-between">
 				{List.map((elem) => {
-					return <span key={elem.name}>{elem.name}</span>;
+					return (
+						<span key={elem.name} className="flex items-center justify-center h-full gap-2">
+							{elem.name} <i className="ri-arrow-down-s-line text-2xl text-black cursor-pointer"></i>
+						</span>
+					);
 				})}
 			</div>
 		</div>
